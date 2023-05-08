5d2928f930a1ed5df5036c4fbc9d7dda64442f74
Eason <Eason9630@gmail.com>
2023-05-08 23:33:38 +0800

    利用程式寫 分頁 縮放

--- PageAndScroll/ViewController.swift
+++ PageAndScroll/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imageViews: [UIImageView]!
+    
+    @IBOutlet var pageView: UIView!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changePage(_ sender: UIPageControl) {
+        let point = CGPoint(x: scrollView.bounds.width * CGFloat(sender.currentPage), y: 0)
+        scrollView.setContentOffset(point, animated: true)
+    }
 }
 
+extension ViewController:UIScrollViewDelegate{
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        pageControl.currentPage = Int(page)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        for pageView in scrollView.subviews{
+            if pageView.isKind(of: UIView.self){
+                return pageView
+            }
+        }
+        return nil
+    }
+
+}
